2f15cf0be47cc2777d2f4fd203722a3eda63267d
Achmad Hanafy <achmadhanafy@gmail.com>
2025-09-20 15:20:49 +0700

    feat: try deploy it into vercel

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import multer from 'multer';
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 import { audioPromptController, chatController, documentPromptController, imagePromptController, textPromptController } from './controllers/gemini.controller.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 const upload = multer({
@@ -14,7 +19,12 @@ const app = express();
 // initial backend
 app.use(cors());
 app.use(express.json());
-app.use(express.static('client/build'));
+// Serve React build
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 app.post('/api/generate-text', textPromptController);
 app.post('/api/chat', chatController);
